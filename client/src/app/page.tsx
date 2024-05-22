5e422afadd4b4fd34f9914324d8b569894a6d5bd
Priyanshu Singh <singhpriyanshu305@gmail.com>
2024-05-23 00:56:22 +0530

    search Input Added

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,12 +2,12 @@ import TransactionPage from "./transactionPage";
 
 export default function Home() {
   return (
-<div className="flex min-h-screen flex-col items-center justify-between p-24">
-    <div className="rounded-full flex flex-col justify-center items-center bg-zinc-900 dark:bg-white w-[12rem] h-[12rem]">
-      <span className="text-2xl font-bold dark:text-zinc-900 text-white ">
+<div className="flex min-h-screen flex-col items-center justify-between p-12">
+    <div className="rounded-full p-15 flex flex-col justify-center items-center bg-zinc-900 dark:bg-white w-[6rem] h-[6rem]">
+      <span className="text-inherit font-bold dark:text-zinc-900 text-white ">
         Transaction
       </span>
-      <span className="text-xl font-bold dark:text-zinc-900 text-white">
+      <span className="text-inherit font-bold dark:text-zinc-900 text-white">
         Dashboard
       </span>
     </div>
--- client/src/app/transactionPage.tsx
+++ client/src/app/transactionPage.tsx
@@ -1,11 +1,10 @@
 "use client";
 
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
@@ -13,28 +12,100 @@ import {
 } from "@/components/ui/table";
 import Image from "next/image";
 import { Badge } from "@/components/ui/badge";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 const TransactionPage = () => {
-  const [data, setData] = useState({});
-  
+  interface Transaction {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    dateOfSale: Date;
+    category: string;
+    image: string;
+    sold: boolean;
+  }
+
+  interface Data {
+    transactions: Transaction[];
+  }
+
+  const monthData = [
+    { name: "January", value: "01" },
+    { name: "February", value: "02" },
+    { name: "March", value: "03" },
+    { name: "April", value: "04" },
+    { name: "May", value: "05" },
+    { name: "June", value: "06" },
+    { name: "July", value: "07" },
+    { name: "August", value: "08" },
+    { name: "September", value: "09" },
+    { name: "October", value: "10" },
+    { name: "November", value: "11" },
+    { name: "December", value: "12" },
+  ];
+ const [data, setData] = useState<Data | null>(null);
+  const [position, setPosition] = useState("03");
+  const [searchInputValue, setSearchInputValue] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await axios.get(
-        "http://localhost:5454/api/combined-data?month=01"
+        `http://localhost:5454/api/combined-data?month=${position}`
       );
       setData(response.data);
     };
     fetchData();
-  }, []);
+    console.log(position);
+  }, [,position]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        `http://localhost:5454/api/transactions?month=${position}&search=${searchInputValue}&page=1&perPage=10`
+      );
+      setData(response.data);
+    }
+
+  },[searchInputValue]);
 
   return (
     <div className="flex flex-col justify-center items-center">
-      <p>{JSON.stringify(data.transactions)}</p>
       <div className="flex flex-col justify-center items-center w-[80vw]">
-          <h1 className="text-2xl font-bold">Transactions</h1>
+        <h1 className="text-2xl font-bold">Transactions</h1>
+        <div className="flex flex-row justify-between w-[80vw] ">
+        <Input type="text" value={searchInputValue} onChange={(e) => setSearchInputValue(e.target.value)} placeholder="Search" className="w-[15vw]" />
         <div>
-          
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline">Select Month</Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+              <DropdownMenuLabel>Select Month</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuRadioGroup
+                value={position}
+                onValueChange={setPosition}
+              >
+                {monthData.map((month) => (
+                  <DropdownMenuRadioItem value={month.value} key={month.value}>
+                    {month.name}
+                  </DropdownMenuRadioItem>
+                ))}
+              </DropdownMenuRadioGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
         </div>
         <Table>
           <TableHeader>
@@ -49,19 +120,22 @@ const TransactionPage = () => {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {data.transactions &&
-              data.transactions.map((transaction: any) => (
+            {data?.transactions &&
+              data?.transactions.map((transaction: Transaction) => (
                 <TableRow key={transaction.id}>
                   <TableCell className="">{transaction.id}</TableCell>
                   <TableCell>{transaction.title}</TableCell>
                   <TableCell>{transaction.description}</TableCell>
-                  <TableCell className="">{transaction.price}</TableCell>
+                  <TableCell className="">₹{transaction.price}</TableCell>
                   <TableCell className="">{transaction.category}</TableCell>
                   <TableCell className="">
                     {transaction.sold ? (
                       <Badge variant="secondary">Sold</Badge>
                     ) : (
-                      <Badge variant="destructive">
+                      <Badge
+                        className="text-xs whitespace-nowrap   "
+                        variant="destructive"
+                      >
                         <p>Not Sold</p>
                       </Badge>
                     )}
